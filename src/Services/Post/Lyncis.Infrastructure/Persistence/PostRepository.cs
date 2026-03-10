de066fc8e2a73fdce2037cba1a6a9571ee83f3fa
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-03-10 19:36:57 -0300

    infrastructure project and post repository added

---
+++ src/Services/Post/Lyncis.Infrastructure/Persistence/PostRepository.cs
@@ -0,0 +1,34 @@
+﻿using Lyncis.Domain.Entities;
+using Lyncis.Domain.Interfaces;
+
+namespace Lyncis.Infrastructure.Persistence
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly Dictionary<Guid, Post> _posts = [];
+
+        public Task<Post?> GetByIdAsync(Guid id)
+        {
+            _posts.TryGetValue(id, out var post);
+            return Task.FromResult(post);
+        }
+
+        public Task AddAsync(Post post)
+        {
+            _posts[post.Id] = post;
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(Post post)
+        {
+            _posts[post.Id] = post;
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            _posts.Remove(id);
+            return Task.CompletedTask;
+        }
+    }
+}
