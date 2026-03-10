41e05b5e8c986c7fcc07b08101d2e0eae752995c
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-03-10 20:03:13 -0300

    add post controller

---
+++ src/Services/Post/Lyncis.Post.Api/Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using Lyncis.Application.Posts.Commands.CreatePost;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lyncis.Post.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostsController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreatePostCommand command)
+        {
+            var postId = await mediator.Send(command);
+            return Ok(new { Id = postId });
+        }
+    }
+}
