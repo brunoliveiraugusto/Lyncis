c2acd0ac1142aff25b466935c3078e18295ac86c
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-03-14 16:04:25 -0300

    add user repository

---
+++ src/Identity/Lyncis.Identity.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lyncis.Identity.Domain.Entities;
+
+namespace Lyncis.Identity.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(Guid id);
+        Task UpdateUserNameAsync(Guid userId, string newName);
+    }
+}
---
+++ src/Identity/Lyncis.Identity.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Lyncis.Identity.Domain.Entities;
+using Lyncis.Identity.Domain.Interfaces;
+
+namespace Lyncis.Identity.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserNameAsync(Guid userId, string newName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
