057981f0e1a2ceb15d4b67262c4961c73cfe2936
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-03-14 16:03:12 -0300

    add aggregate root

---
+++ src/Identity/Lyncis.Identity.Domain/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿namespace Lyncis.Identity.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public User(string name, string email)
+        {
+            ValidateName(name);
+            ValidateEmail(email);
+
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+        public void UpdateName(string newName)
+        {
+            ValidateName(newName);
+
+            Name = newName;
+        }
+
+        private static void ValidateName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                throw new ArgumentException("Name cannot be empty.");
+        }
+
+        private static void ValidateEmail(string email)
+        {
+            if (!string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email cannot be empty.");
+        }
+    }
+}
