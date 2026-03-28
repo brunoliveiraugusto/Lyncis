e794a80908684a2f2a362c81d745f753e20eaa82
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-03-28 13:02:08 -0300

    rename

--- src/Identity/Lyncis.Identity.Application/Common/Behaviors/TransactionBehavior.cs
+++ src/Identity/Lyncis.Identity.Application/Common/Behaviors/TransactionBehavior.cs
@@ -5,26 +5,26 @@ namespace Lyncis.Identity.Application.Common.Behaviors
 {
     internal class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _uow;
 
-        public TransactionBehavior(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public TransactionBehavior(IUnitOfWork uow) => _uow = uow;
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
         {
-            await _unitOfWork.BeginTransactionAsync(ct);
+            await _uow.BeginTransactionAsync(ct);
 
             try
             {
                 var response = await next();
                 
-                await _unitOfWork.SaveChangesAsync(ct);
-                await _unitOfWork.CommitTransactionAsync(ct);
+                await _uow.SaveChangesAsync(ct);
+                await _uow.CommitTransactionAsync(ct);
 
                 return response;
             }
             catch (Exception)
             {
-                await _unitOfWork.RollbackTransactionAsync(ct);
+                await _uow.RollbackTransactionAsync(ct);
 
                 throw;
             }
