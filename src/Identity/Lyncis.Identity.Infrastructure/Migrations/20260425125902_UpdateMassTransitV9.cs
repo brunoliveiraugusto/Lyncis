460e171aa937936501c58568946bd57d0bfe4ec8
Bruno Augusto de Souza Oliveira <bruno.oliveira@dev.audaces.local>
2026-04-25 10:41:07 -0300

    update mass transit v9

---
+++ src/Identity/Lyncis.Identity.Infrastructure/Migrations/20260425125902_UpdateMassTransitV9.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lyncis.Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMassTransitV9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BusName",
+                table: "OutboxState",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxState_BusName_Created",
+                table: "OutboxState",
+                columns: new[] { "BusName", "Created" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_OutboxState_BusName_Created",
+                table: "OutboxState");
+
+            migrationBuilder.DropColumn(
+                name: "BusName",
+                table: "OutboxState");
+        }
+    }
+}
